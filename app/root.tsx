982a94eeb35f3bb2c0d2e172b35cc6e42f34880c
jmwalsh91 <jordan@jmwalsh.dev>
2025-02-04 21:07:13 -0500

    chore: rebuild with new vercel config

--- app/root.tsx
+++ app/root.tsx
@@ -37,6 +37,7 @@ export const links: Route.LinksFunction = () => [
   { rel: "manifest", href: "/site.webmanifest" },
 ];
 
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
